eb88bf6953b3b87bcc0b1f4395926b02a06d05db
roytan883 <blueness883@163.com>
2017-12-28 17:37:21 +0800

    can use image

--- src/pages/homePage/index.js
+++ src/pages/homePage/index.js
@@ -3,11 +3,14 @@ import './index.less'
 
 import { Link } from 'react-router-dom'
 
+import img1 from '../../static/img/flexed-biceps_1f4aa.png';
+
 export default class Home extends Component {
   render() {
     return (
       <div>
         Home
+        <img src={img1} alt="hi" />
         <Link to="/about">About</Link>
       </div>
     )
